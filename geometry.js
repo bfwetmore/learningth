26f9ec2de20e79e89c346da928a6e8bb0e1be1bb
bfwetmore <bfwetmore@gmail.com>
2021-11-09 22:23:16 -0500

    Added Comments to the geometry functions.

--- geometry.js
+++ geometry.js
@@ -1,16 +1,42 @@
+/**
+ * Calculates
+ * @param width - the width of a rectangle
+ * @param height - the height of a rectangle
+ * @returns {number} - the area of the rectangle.
+ */
 
 function rect(width, height) {
     return width * height;
 }
 
+/**
+ * Calculates and Returns the area of a prism.
+ *
+ * @param width - the width of a prism
+ * @param height - the height of a prism
+ * @param length - the length of a prism
+ * @returns {number} - the area of a prism
+ */
 function prism(width, height, length) {
     return width * height * length;
 }
 
+/**
+ * Calculates and returns the area of a rectangle
+ *
+ * @param radius - the radius of a circle
+ * @returns {number} - the area of a circle
+ */
 function circle(radius) {
     return Math.PI * Math.pow(radius, 2);
 }
 
+/**
+ * Calculates and returns the area of a Sphere
+ *
+ * @param radius
+ * @returns {number}
+ */
 function sphere(radius) {
     return (4 / 3) * Math.PI * Math.pow(radius, 3);
 }
