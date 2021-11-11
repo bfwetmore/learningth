8ac5d3120cd9b105f6861bd9b0a55c08ea95926e
Benjamin Wetmore <ben@VenomMAC.local>
2021-11-10 23:15:17 -0500

    Updated index.html also added a few loop lesson work files.

---
+++ do-while.js
@@ -0,0 +1,7 @@
+function getRandomNumber(upper) {
+    return Math.floor( Math.random() * upper ) + 1;
+}
+let counter = 0;
+do { console.log(`Here is a random number ${getRandomNumber(10)}`)
+counter ++;
+} while (counter < 1);
