95b4d55d13dd629f1b3c049b5c6b96f91975a03d
bfwetmore <bfwetmore@gmail.com>
2021-11-19 23:37:16 -0500

    Address Comments.
    -All lowercase on directory files.
    -Added additional line at the end of each file.

--- functions/geometry.js
+++ functions/geometry.js
@@ -45,4 +45,3 @@ console.log(rect(5, 22));
 console.log(prism(4.5, 12.5, 17.4));
 console.log(circle(7.2));
 console.log(sphere(7.2));
-
--- loops/Loops Practice/js/app.js
+++ loops/Loops Practice/js/app.js
@@ -23,8 +23,3 @@ main.innerHTML = `<h1>CORRECT! It took you ${i} times to guess ${randomNumber} <
 if (i === 10) {
     main.innerHTML = `<h1>Too many tries, the answer was ${randomNumber}.`
 }
-
-// TODO: Use a loop to create a number guessing game
-//  1) Ask the user to enter a number and assign that value to the `guess` variable
-//  2) End the loop when the user's guess matches the random number
-//  3) Display a message letting the user know they guessed the number
