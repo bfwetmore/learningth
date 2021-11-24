b08c4320c4252f017bb0089eec2099d366b49d8d
bfwetmore <bfwetmore@gmail.com>
2021-11-23 22:53:37 -0500

    Add work and challenge From array Lesson.

--- array's/build-a-quiz-challenge/start/js/quiz.js
+++ array's/build-a-quiz-challenge/start/js/quiz.js
@@ -1,30 +1,42 @@
-// 1. Create a multidimensional array to hold quiz questions and answers
+/**
+ * Create a list of Imported Arrays.
+ * @param mon The Parameter for Array Argument.
+ * @returns {string} A list of string values, in HTML list format.
+ */
+
+function createList(mon) {
+    let list = ''
+    for (let i = 0; i < mon.length; i++) {
+        list += `<li>${mon[i]}</li>`
+    }
+    return list;
+}
+
+// Arrays
 const quiz = [
     ['What is my age?', '31'],
     ['What is my favorite color?', 'red'],
     ['What town did I grow up in?', 'bantam']
 ]
+//Write or Wrong answer arrays in quiz
+let right = [];
+let wrong = [];
+let correct = 0;
 
-// 2. Store the number of questions answered correctly
-let correct = 0
-
-/* 
-  3. Use a loop to cycle through each question
-      - Present each question to the user
-      - Compare the user's response to answer in the array
-      - If the response matches the answer, the number of correctly
-        answered questions increments by 1
-*/
 for (let i = 0; i < quiz.length; i++) {
-    let answer = (prompt(quiz[i][0]))
+    let answer = prompt(quiz[i][0])
     if (answer === quiz[i][1]) {
-        correct++
+        correct++;
+        right.push(quiz[i][0]);
+        continue;
     }
-}
+    wrong.push(quiz[i][0])
 
+}
 document.querySelector("main").innerHTML = `
 <h1>You got ${correct} question(s) correct</h1>
+<h2>You got these question(s) right!</h2><
+<ol>${createList(right)}</ol>
+<h2>You got these question(s) wrong :(</h2>
+<ol>${createList(wrong)}</ol>
 `
-
-
-// 4. Display the number of correct answers to the user
