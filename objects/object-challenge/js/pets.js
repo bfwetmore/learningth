1ea23e9965d9ac6b97c41496dff9d140a1a49eb5
bfwetmore <bfwetmore@gmail.com>
2021-12-02 21:41:12 -0500

    Make changes according to comments.

--- objects/object-challenge/js/pets.js
+++ objects/object-challenge/js/pets.js
@@ -1,47 +1,49 @@
+const path = ('img/');
 const pets = [
     {
         name: 'Sam',
         type: 'Dog',
         breed: 'Australian Shepard',
         age: 6,
-        photo: 'img/aussie.jpg'
+        photo: 'aussie.jpg'
     },
     {
         name: 'Bailey',
         type: 'Dog',
         breed: 'Dachshund',
         age: 4,
-        photo: 'img/dachshund.jpg'
+        photo: 'dachshund.jpg'
     },
     {
         name: 'Goldy',
         type: 'Dog',
         breed: 'Golden Retriever',
         age: 12,
-        photo: 'img/golden.jpg'
+        photo: 'golden.jpg'
     },
     {
         name: 'Amdi',
         type: 'Cat',
         breed: 'Persian',
         age: 10,
-        photo: 'img/persian.jpg'
+        photo: 'persian.jpg'
     },
     {
         name: 'Ellie',
         type: 'Dog',
         breed: 'Pug',
         age: 9,
-        photo: 'img/pug.jpg'
+        photo: 'pug.jpg'
     },
     {
         name: 'Tabby',
         type: 'Cat',
         breed: 'Mixed',
         age: 2,
-        photo: 'img/tabby.jpg'
+        photo: 'tabby.jpg'
     }
 ];
+
 let html = '';
 for (let i = 0; i < pets.length; i++) {
     console.log(pets[i].name);
@@ -49,7 +51,8 @@ for (let i = 0; i < pets.length; i++) {
         <h2>${pets[i].name}</h2>
         <h3>${pets[i].type} | ${pets[i].breed}</h3>
         <p>Age: ${pets[i].age}</p>
-        <img src="${pets[i].photo}" alt="${pets[i].breed}">
+        <img src="${path}${pets[i].photo}" alt="${pets[i].breed}">
       `;
 }
+
 document.querySelector("main").innerHTML = html;
