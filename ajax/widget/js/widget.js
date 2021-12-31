860c8039c3c22b7f690ca9cf1af12ac57f8d57ef
bfwetmore <bfwetmore@gmail.com>
2021-12-30 23:04:49 -0500

    Make changes according to comments, other improvements added as well.

--- ajax/widget/js/widget.js
+++ ajax/widget/js/widget.js
@@ -1,41 +1,40 @@
 let employeeList = new XMLHttpRequest();
-employeeList.onreadystatechange = function () {
-    if (employeeList.readyState === 4) {
-        let employees = JSON.parse(employeeList.responseText);
-        let employeeStatus = '<ul class="bulleted">';
-        for (let i = 0; i < employees.length; i++) {
-            if (employees[i].inoffice === true) {
-                employeeStatus += '<li class="in">';
-            } else {
-                employeeStatus += '<li class="out">';
-            }
-            employeeStatus += employees[i].name;
-            employeeStatus += '</li>';
+
+employeeList.onreadystatechange = () => {
+    if (employeeList.readyState === 4) {return;}
+    let employees = JSON.parse(employeeList.responseText);
+    let employeeStatus = '<ul class="bulleted">';
+    for (let i = 0; i < employees.length; i++) {
+        let employeeStatusClass = "out";
+        if (employees[i].inoffice === true) {
+            employeeStatusClass = 'in';
         }
-        employeeStatus += '</ul>';
-        document.getElementById('employeeList').innerHTML = employeeStatus;
+        employeeStatus += `<li class="${employeeStatusClass}">
+        ${employees[i].name}</li>`;
     }
+    employeeStatus += '</ul>';
+    document.getElementById('employeeList').innerHTML = employeeStatus;
 };
+
 employeeList.open('GET', 'data/employees.json');
 employeeList.send();
 
 let roomList = new XMLHttpRequest();
-roomList.onreadystatechange = function () {
-    if (roomList.readyState === 4) {
-        let openRooms = JSON.parse(roomList.responseText);
-        let roomStatus = '<ul class="rooms">';
-        for (let i = 0; i < openRooms.length; i++) {
-            if (openRooms[i].available === true) {
-                roomStatus += '<li class="empty">';
-            } else {
-                roomStatus += '<li class="full">';
-            }
-            roomStatus += openRooms[i].roomNumber;
-            roomStatus += '</li>';
+roomList.onreadystatechange = () => {
+    if (roomList.readyState === 4) {return;}
+    let openRooms = JSON.parse(roomList.responseText);
+    let roomStatus = '<ul class="rooms">';
+    for (let i = 0; i < openRooms.length; i++) {
+        let roomStatusClass = 'full';
+        if (openRooms[i].available === true) {
+            roomStatusClass = "empty";
         }
-        roomStatus += '</ul>';
-        document.getElementById('roomList').innerHTML = roomStatus;
+        roomStatus += `<li class='${roomStatusClass}'>
+        ${openRooms[i].roomNumber}</li>`;
     }
+    roomStatus += '</ul>';
+    document.getElementById('roomList').innerHTML = roomStatus;
 };
+
 roomList.open('GET', 'data/rooms.json');
 roomList.send();
