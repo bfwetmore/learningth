411fb4a84b5681e6273b888d00b96133e109e358
bfwetmore <bfwetmore@gmail.com>
2022-01-24 18:02:25 -0500

    Add Random dog Image application. -Includes supporting HTML and CSS files. -Will generate an image based on animal selection. -Comment and name fields made available for future use.

--- fetch-api/js/app.js
+++ fetch-api/js/app.js
@@ -1,42 +1,70 @@
 const select = document.getElementById('breeds');
 const card = document.querySelector('.card');
-const form = document.querySelector('form');
 
-// ------------------------------------------
-//  FETCH FUNCTIONS
-// ------------------------------------------
-fetch('https://dog.ceo/api/breeds/image/random')
-    .then((response) => response.json())
-    .then(data => generateImage(data.message));
-fetch('https://dog.ceo/api/breeds/list')
-    .then((breedResponse) => breedResponse.json())
-    .then((breedData) => generateBreedOptions(breedData.message));
-// ------------------------------------------
-//  HELPER FUNCTIONS
-// ------------------------------------------
+select.addEventListener('change', fetchBreedImage);
+card.addEventListener('click', fetchBreedImage);
+document.querySelector('form').addEventListener('submit', postInputFieldData);
 
+Promise.all([
+    fetchData('https://dog.ceo/api/breeds/image/random'),
+    fetchData('https://dog.ceo/api/breeds/list')
+])
+    .then(data => {
+        generateImage(data[0].message);
+        generateBreedOptions(data[1].message);
+    });
+
+//Functions:
+function checkStatus(response) {
+    if (response.ok) {
+        return Promise.resolve(response);
+    }
+    return Promise.reject(new Error(response.statusText));
+}
 
 function generateImage(data) {
-    const html = `
-    <img src='${data}' alt>
-    <p>Click to view images of ${select.value}s</p>
-    `;
-    card.innerHTML = html;
+    card.innerHTML = `
+        <img src='${data}' alt>
+        <p>Click to view images of ${select.value}s</p>
+        `;
 }
 
-function generateBreedOptions(breedData) {
-    const options = breedData.map(item => `
-<option value='${item}'>${item}</option>
-`).join("");
-    select.innerHTML = options;
+function generateBreedOptions(data) {
+    select.innerHTML = data.map(item => `
+        <option value='${item}'>${item}</option>
+        `).join("");
 }
 
-// ------------------------------------------
-//  EVENT LISTENERS
-// ------------------------------------------
+function fetchBreedImage() {
+    const breed = select.value;
+    const img = card.querySelector('img');
 
+    fetchData(`https://dog.ceo/api/breed/${breed}/images/random`)
+        .then(data => {
+            img.src = data.message;
+            img.alt = breed;
+            card.querySelector('p').textContent = `Click to view more ${breed}s`;
+        });
+}
 
-// ------------------------------------------
-//  POST DATA
-// ------------------------------------------
+function postInputFieldData(e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const comment = document.getElementById('comment').value;
+    const postConfig = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name, comment})
+    };
+    fetchData('https://jsonplaceholder.typicode.com/comments', postConfig)
+        .then(data => console.log(data));
+}
 
+function fetchData(url, request) {
+    return fetch(url, request)
+        .then(checkStatus)
+        .then(response => response.json())
+        .catch(error => console.log('There has been an error!', error));
+}
