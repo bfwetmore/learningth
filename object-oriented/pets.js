3148d2b5c8cec0efb6e7b34ed7e89b4bb2eca389
bfwetmore <bfwetmore@gmail.com>
2022-01-25 20:35:26 -0500

    WIP

---
+++ object-oriented/pets.js
@@ -0,0 +1,16 @@
+const ernie = {
+    animal: 'dog',
+    age: '1',
+    breed: 'pug',
+    bark: function(){
+        console.log('Woof!');
+    }
+}
+
+ernie.age = 2
+
+console.log(ernie.age)
+
+ernie.color = 'black';
+
+console.log(ernie)
