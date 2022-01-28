782c16667aaffc7dc4c62f1da84287b112b48b3e
bfwetmore <bfwetmore@gmail.com>
2022-01-27 22:31:51 -0500

    Make adjustments according to change requests.

--- fetch-api/js/app.js
+++ fetch-api/js/app.js
@@ -3,7 +3,7 @@ const breedImgCard = document.querySelector('.card');
 
 selectBreedElement.addEventListener('change', updateBreedValue);
 breedImgCard.addEventListener('click', updateBreedValue);
-document.querySelector('form').addEventListener('submit', postInputFieldData);
+document.querySelector('form').addEventListener('submit', getElementInput);
 
 Promise.all([
     fetchData('https://dog.ceo/api/breeds/image/random'),
@@ -14,17 +14,10 @@ Promise.all([
         generateBreedOptionsElement(data[1].message);
     });
 
-function updateBreedValue() {
-    let breed = selectBreedElement.value;
-    console.log(breed);
-    let img = breedImgCard.querySelector('img');
-    return fetchBreedImage(img, breed);
-}
-
 /**
  * Checks for errors in fetch requests.
- * @param response
- * @returns {Promise<never>|Promise<unknown>}
+ * @param response Response from fetchData method
+ * @returns Back to method, or returns error in console.
  */
 function checkStatus(response) {
     if (response.ok) {
@@ -35,7 +28,7 @@ function checkStatus(response) {
 
 /**
  * Imports an image url onto the DOM
- * @param data
+ * @param data Random IMG JSON
  */
 function generateImage(data) {
     breedImgCard.innerHTML = `
@@ -46,7 +39,7 @@ function generateImage(data) {
 
 /**
  * Utilizes JSON data to create options in Dropdown box
- * @param data
+ * @param data Pet Breed list API JSON data.
  */
 function generateBreedOptionsElement(data) {
     selectBreedElement.innerHTML = data.map(item => `
@@ -55,7 +48,17 @@ function generateBreedOptionsElement(data) {
 }
 
 /**
- * Enforces the image to only show user selected breed.
+ * Updates the variables currently selected breed in the dropdown box.
+ */
+function updateBreedValue() {
+    let breed = selectBreedElement.value;
+    console.log(breed);
+    let img = breedImgCard.querySelector('img');
+    return fetchBreedImage(img, breed);
+}
+
+/**
+ * Fetches breed images JSON data from API
  */
 function fetchBreedImage(img, breed) {
     fetchData(`https://dog.ceo/api/breed/${breed}/images/random`)
@@ -64,6 +67,12 @@ function fetchBreedImage(img, breed) {
         });
 }
 
+/**
+ * Edits the DOM according to the currently selected breed.
+ * @param data Random generated URL extracted from API
+ * @param img The URL to be displayed
+ * @param breed The name to be show in the text below the image.
+ */
 function editDom(data, img, breed) {
     img.src = data.message;
     img.alt = breed;
@@ -71,29 +80,37 @@ function editDom(data, img, breed) {
 }
 
 /**
- * Collects user input and submits in a POST request.
- * @param e
+ * Takes user input and prepares a POST request
+ * @param e Event listener to prevent Default of page reload after form Submission
  */
-function postInputFieldData(e) {
+function getElementInput(e){
     e.preventDefault();
-    const name = document.getElementById('name').value;
-    const comment = document.getElementById('comment').value;
+    const nameFormElement = document.getElementById('name').value;
+    const commentFormElement = document.getElementById('comment').value;
     const postConfig = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({name, comment})
+        body: JSON.stringify({name: nameFormElement, comment: commentFormElement})
     };
+    return postInputFieldData(postConfig)
+}
+
+/**
+ * Sends POST request.
+ * @param postConfig Assembled POST request
+ */
+function postInputFieldData(postConfig) {
     fetchData('https://jsonplaceholder.typicode.com/comments', postConfig)
         .then(data => console.log(data));
 }
 
 /**
  * Reusable function for multiple Fetch API's.
- * @param url
- * @param request
- * @returns {Promise<any>}
+ * @param url The API
+ * @param request optional parameter for request type
+ * @returns {Promise<any>}API info parsed into JSON format.
  */
 function fetchData(url, request = null) {
     return fetch(url, request)
